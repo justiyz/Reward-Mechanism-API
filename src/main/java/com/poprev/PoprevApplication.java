9a2c9280177ef5a909b1f9709e3726917682c3ae
Ernest <inyange8888@gmail.com>
2023-07-01 19:13:00 +0100

    first commit

---
+++ src/main/java/com/poprev/PoprevApplication.java
@@ -0,0 +1,13 @@
+package com.poprev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoprevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoprevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poprev/PoprevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poprev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoprevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
